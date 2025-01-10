e7de42e990af75d7c780dc68026b573c03012534
Mykyta Kondratiev <kondratiev.mikita@gmail.com>
2025-01-10 14:19:31 -0800

    Error handling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-// import profilePic from "../assets/kevinRushProfile.png"; // Update the image if needed
+import profilePic from "../assets/mykyta-profile.png"; // Update the image if needed
 
 const HERO_CONTENT =
   "I am a passionate Data Scientist with over 3 years of experience in applying data-driven solutions to solve complex challenges across diverse industries. Adept in Python, SQL, and big data technologies like Hadoop and Spark, I excel at creating and deploying machine learning models, conducting web scraping, and integrating APIs to support decision-making. My expertise includes building predictive models, deep learning applications, and dynamic visualizations through tools such as Tableau, Streamlit, and Power BI. I thrive on using cutting-edge tools and data science techniques to optimize processes, improve operational efficiency, and deliver actionable insights that drive measurable business outcomes.";
