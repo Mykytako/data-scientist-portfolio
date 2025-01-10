0d9d4d56135f67228a7ea3d55d4354e27f3fe944
Mykyta Kondratiev <kondratiev.mikita@gmail.com>
2025-01-10 14:21:06 -0800

    Error handling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 // import aboutImg from "../assets/about.jpg";
-import { ABOUT_TEXT } from "../constants";
+// import { ABOUT_TEXT } from "../constants";
 import { motion } from "framer-motion";
 
 const About = () => {
@@ -26,9 +26,9 @@ const About = () => {
           transition={{ duration: 0.5 }}
           className="w-full lg:w-1/2"
         >
-          <div className="flex justify-center lg:justify-start">
+          {/* <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6">{ABOUT_TEXT}</p>
-          </div>
+          </div> */}
         </motion.div>
       </div>
     </div>
